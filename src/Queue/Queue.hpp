14e2b1edcac2acc673358d50cca5a7a33909b26f
Aristotelis Bobas <abobas@student.codam.nl>
2020-08-23 18:27:39 +0200

    style

--- src/Queue/Queue.hpp
+++ src/Queue/Queue.hpp
@@ -6,7 +6,7 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/06 16:49:33 by abobas        #+#    #+#                 */
-/*   Updated: 2020/08/17 18:11:39 by abobas        ########   odam.nl         */
+/*   Updated: 2020/08/23 18:27:11 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,73 +17,74 @@
 
 namespace ft
 {
-template <class T, class Container = list<T> >
+
+template <class T, class Container = list<T>>
 class queue
 {
 public:
-    typedef T value_type;
-    typedef Container container_type;
-    typedef size_t size_type;
+	typedef T value_type;
+	typedef Container container_type;
+	typedef size_t size_type;
 
-    explicit queue(const container_type &cntr = container_type())
-    {
-        this->container = cntr;
-    }
+	explicit queue(const container_type &cntr = container_type())
+	{
+		this->container = cntr;
+	}
 
-    bool empty() const
-    {
-        return (this->container.empty());
-    }
+	bool empty() const
+	{
+		return (this->container.empty());
+	}
 
-    size_type size() const
-    {
-        return (this->container.size());
-    }
+	size_type size() const
+	{
+		return (this->container.size());
+	}
 
-    value_type &front()
-    {
-        return (this->container.front());
-    }
+	value_type &front()
+	{
+		return (this->container.front());
+	}
 
-    const value_type &front() const
-    {
-        return (this->container.front());
-    }
+	const value_type &front() const
+	{
+		return (this->container.front());
+	}
 
-    value_type &back()
-    {
-        return (this->container.back());
-    }
+	value_type &back()
+	{
+		return (this->container.back());
+	}
 
-    const value_type &back() const
-    {
-        return (this->container.back());
-    }
+	const value_type &back() const
+	{
+		return (this->container.back());
+	}
 
-    void push(const value_type &val)
-    {
-        this->container.push_back(val);
-    }
+	void push(const value_type &val)
+	{
+		this->container.push_back(val);
+	}
 
-    void pop()
-    {
-        this->container.pop_front();
-    }
+	void pop()
+	{
+		this->container.pop_front();
+	}
 
-    template <class U, class Cont>
-    friend bool operator==(const queue<U, Cont> &lhs, const queue<U, Cont> &rhs);
+	template <class U, class Cont>
+	friend bool operator==(const queue<U, Cont> &lhs, const queue<U, Cont> &rhs);
 
-    template <class U, class Cont>
-    friend bool operator<(const queue<U, Cont> &lhs, const queue<U, Cont> &rhs);
+	template <class U, class Cont>
+	friend bool operator<(const queue<U, Cont> &lhs, const queue<U, Cont> &rhs);
 
 private:
-    container_type container;
+	container_type container;
 };
 
 template <class T, class Container>
 bool operator==(const queue<T, Container> &lhs, const queue<T, Container> &rhs)
 {
-    return (lhs.container == rhs.container);
+	return (lhs.container == rhs.container);
 }
 
 template <class T, class Container>
@@ -95,7 +96,7 @@ bool operator!=(const queue<T, Container> &lhs, const queue<T, Container> &rhs)
 template <class T, class Container>
 bool operator<(const queue<T, Container> &lhs, const queue<T, Container> &rhs)
 {
-    return (lhs.container < rhs.container);
+	return (lhs.container < rhs.container);
 }
 
 template <class T, class Container>
--- src/Stack/Stack.hpp
+++ src/Stack/Stack.hpp
@@ -6,7 +6,7 @@
 /*   By: abobas <abobas@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/08/06 16:49:33 by abobas        #+#    #+#                 */
-/*   Updated: 2020/08/17 18:11:30 by abobas        ########   odam.nl         */
+/*   Updated: 2020/08/23 18:27:14 by abobas        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,63 +17,64 @@
 
 namespace ft
 {
-template <class T, class Container = list<T> >
+
+template <class T, class Container = list<T>>
 class stack
 {
 public:
-    typedef T value_type;
-    typedef Container container_type;
-    typedef size_t size_type;
+	typedef T value_type;
+	typedef Container container_type;
+	typedef size_t size_type;
 
-    explicit stack(const container_type &cntr = container_type())
-    {
-        this->container = cntr;
-    }
+	explicit stack(const container_type &cntr = container_type())
+	{
+		this->container = cntr;
+	}
 
-    bool empty() const
-    {
-        return (this->container.empty());
-    }
+	bool empty() const
+	{
+		return (this->container.empty());
+	}
 
-    size_type size() const
-    {
-        return (this->container.size());
-    }
+	size_type size() const
+	{
+		return (this->container.size());
+	}
 
-    value_type &top()
-    {
-        return (this->container.back());
-    }
+	value_type &top()
+	{
+		return (this->container.back());
+	}
 
-    const value_type &top() const
-    {
-        return (this->container.back());
-    }
+	const value_type &top() const
+	{
+		return (this->container.back());
+	}
 
-    void push(const value_type &val)
-    {
-        this->container.push_back(val);
-    }
+	void push(const value_type &val)
+	{
+		this->container.push_back(val);
+	}
 
-    void pop()
-    {
-        this->container.pop_back();
-    }
+	void pop()
+	{
+		this->container.pop_back();
+	}
 
-    template <class U, class Cont>
-    friend bool operator==(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs);
+	template <class U, class Cont>
+	friend bool operator==(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs);
 
-    template <class U, class Cont>
-    friend bool operator<(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs);
+	template <class U, class Cont>
+	friend bool operator<(const stack<U, Cont> &lhs, const stack<U, Cont> &rhs);
 
 private:
-    container_type container;
+	container_type container;
 };
 
 template <class T, class Container>
 bool operator==(const stack<T, Container> &lhs, const stack<T, Container> &rhs)
 {
-    return (lhs.container == rhs.container);
+	return (lhs.container == rhs.container);
 }
 
 template <class T, class Container>
@@ -85,7 +86,7 @@ bool operator!=(const stack<T, Container> &lhs, const stack<T, Container> &rhs)
 template <class T, class Container>
 bool operator<(const stack<T, Container> &lhs, const stack<T, Container> &rhs)
 {
-    return (lhs.container < rhs.container);
+	return (lhs.container < rhs.container);
 }
 
 template <class T, class Container>
